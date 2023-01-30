d76bf5835e0c9a5dd64dc1ca6b2cee2a1e9a65f5
OSauceda <osfesa462@gmail.com>
2023-01-30 14:34:39 -0600

    - Created new container for session form.

--- src/App.js
+++ src/App.js
@@ -1,97 +1,10 @@
-import './App.css';
-import { Button, DatePicker, Form, Input, TimePicker } from 'antd';
-import TextArea from 'antd/es/input/TextArea';
-import SignatureComponent from './components/SignaturePad/SignaturePad';
-import { useRef } from 'react';
-import axios from 'axios';
-import ListField from './components/ListField/ListField';
-
-const onFinish = (values) => {
-  console.log('success', values);
-  axios
-    .post('http://localhost:3001/session-reports', values)
-    .then((res) => console.log('success', res))
-    .catch((error) => console.log('error', error));
-};
-
-const onFinishFailed = (errorInfo) => {
-  console.log('Failed:', errorInfo);
-};
+import "./App.css";
+import SessionForm from "./containers/SessionForm";
 
 function App() {
-  const engineerSignRef = useRef();
-  const [form] = Form.useForm();
-
   return (
     <div className="App" style={{ margin: 20 }}>
-      <h1>Embassy Studios Session Report</h1>
-      <Form
-        form={form}
-        name="form"
-        initialValues={{}}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item name="date" label="Date">
-          <DatePicker />
-        </Form.Item>
-        <Form.Item name="startAndEndTime" label="Start/End Time">
-          <TimePicker.RangePicker />
-        </Form.Item>
-        <Form.Item name="artist" label="Artist">
-          <Input />
-        </Form.Item>
-        <Form.Item name="label" label="Label">
-          <Input />
-        </Form.Item>
-        <Form.Item name="personWhoBooked" label="Person Who Booked">
-          <Input />
-        </Form.Item>
-        <Form.Item name="engineer" label="Engineer">
-          <Input />
-        </Form.Item>
-        <Form.Item name="assistantEngineer" label="Assistant Engineer">
-          <Input />
-        </Form.Item>
-        <Form.Item name="featuredArtists" label="Featured Artists">
-          <Input />
-        </Form.Item>
-        <Form.Item name="sessionName" label="Session Name">
-          <Input />
-        </Form.Item>
-        <Form.Item name="fileLocation" label="File Location">
-          <Input />
-        </Form.Item>
-        <ListField name="micsUsed" title="Mics Used" />
-        <ListField name="preAmpsUsed" title="Pre-Amps Used" />
-        <ListField name="outboardGearUsed" title="Out board Gear Used" />
-        <ListField name="instrumentsUsed" title="Instruments Used" />
-
-        <Form.Item name="sslSessionName" label="SSL Session Name">
-          <Input />
-        </Form.Item>
-        <Form.Item name="engineerSignature" label="Engineer Signature">
-          <SignatureComponent ref={engineerSignRef} padNumber={1} />
-        </Form.Item>
-        <Form.Item
-          name="assistantEngineerSignature"
-          label="Assistant Engineer Signature"
-        >
-          <SignatureComponent padNumber={2} />
-        </Form.Item>
-        <Form.Item name="clientSignature" label="Client Signature">
-          <SignatureComponent padNumber={3} />
-        </Form.Item>
-        <Form.Item name="notes" label="Additional Notes">
-          <TextArea />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+      <SessionForm />
     </div>
   );
 }
