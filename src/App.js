fca7695510d2b08bfb9dab910ed22d8cad157b5e
OSauceda <osfesa462@gmail.com>
2023-02-08 14:38:20 -0600

    - Added header component. - Added embassy logo.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import routes from './router';
-import { Layout, Typography } from 'antd';
+import { Layout, Typography, Image, Row, Col, Button } from 'antd';
 import { RouterProvider } from 'react-router-dom';
 import { LoadingProvider } from './components/Loading/LoadingContext';
 import Loading from './components/Loading/Loading';
 import { AuthProvider } from './context/AuthContext';
+import logo from './image/embassy-logo-transparent.png';
 
-const { Content } = Layout;
+const { Content, Header } = Layout;
 const { Title } = Typography;
 
 const App = () => {
@@ -15,6 +16,18 @@ const App = () => {
       <LoadingProvider>
         <Loading />
         <Layout style={{ minHeight: '100vh' }} className="App">
+          <Header style={{ marginBottom: '2.5rem' }}>
+            <Row align="middle" justify="space-between">
+              <Col>
+                <Image
+                  width="175px"
+                  src={logo}
+                  preview={false}
+                  alt="embassy-studios-logo"
+                />
+              </Col>
+            </Row>
+          </Header>
           <Content>
             <Title>Embassy Studios Session Report</Title>
             <RouterProvider router={routes} />
